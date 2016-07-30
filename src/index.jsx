f4e67dc86c046d2676c1fc4646c3be87c8010ba8
Colm O'Shea <colmoshea@mcqs.com>
2016-07-31 00:31:43 +0100

    Split voting rendering into Winner and Vote jsx files

--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,6 @@ import Voting from './components/Voting';
 const pair = ['Trainspotting', '28 Days Later'];
 
 ReactDOM.render(
-    <Voting pair={pair} />,
+    <Voting pair={pair} winner="Trainspotting"/>,
     document.getElementById('app')
 );
