9893e3c52d584262e0644491885b63efa23a8a53
Colm O'Shea <colmoshea@mcqs.com>
2016-07-31 01:38:38 +0100

    Initial redux-react binding commit

--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
+import {createStore} from 'redux';
 
+import {Provider} from 'react-redux';
+import reducer from './reducer.jsx';
 import App from './components/App';
-import Voting from './components/Voting';
+import {VotingContainer} from './components/Voting';
 import Results from './components/Results';
 //https://github.com/teropa/redux-voting-client/commit/css
 //require('./style.css');
 
+const store = createStore(reducer);
+
+store.dispatch({
+    type: 'SET_STATE',
+    state: {
+        vote: {
+            pair: ['127 Hours', 'Dr.Octopus'],
+            tally: {'127 hours': 3}
+        }
+    }
+});
+
 const routes = <Route component={App}>
         <Route path="/results" component={Results}/>
-        <Route path="/" component={Voting}/>
+        <Route path="/" component={VotingContainer}/>
     </Route>;
 
 ReactDOM.render(
-    <Router history={hashHistory}>{routes}</Router>,
+    <Provider store={store}>
+        <Router history={hashHistory}>{routes}</Router>
+    </Provider>,
     document.getElementById('app')
 );
