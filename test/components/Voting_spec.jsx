3cbc50443f62e253585089ed217d9286bd942044
Colm O'Shea <colmoshea@mcqs.com>
2016-07-31 01:02:27 +0100

    Added check Pure rendering tests

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -7,6 +7,8 @@ import {
     Simulate
 } from 'react-addons-test-utils';
 import Voting from '../../src/components/Voting.jsx';
+
+import {List} from 'immutable';
 import {expect} from 'chai';
 
 describe('voting', () => {
@@ -68,4 +70,45 @@ describe('voting', () => {
         expect(winner).to.be.ok;
         expect(winner.textContent).to.contain('Trainspotting');
     });
+
+    it('renders as a pure component', () => {
+        const pair = ['Trainspotting', '28 Days Later'];
+        const container = document.createElement('div');
+        let component = ReactDOM.render(
+            <Voting pair={pair} />,
+            container
+        );
+
+        let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('Trainspotting');
+
+        pair[0] = 'Sunshine';
+        component = ReactDOM.render(
+            <Voting pair={pair} />,
+            container
+        );
+        firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('Trainspotting');
+    });
+
+    it('does update DOM when prop changes', () => {
+        const pair = List.of('Trainspotting', '28 Days Later');
+        const container = document.createElement('div');
+        let component = ReactDOM.render(
+            <Voting pair={pair} />,
+            container
+        );
+
+        let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('Trainspotting');
+
+        const newPair = pair.set(0, 'Sunshine');
+        component = ReactDOM.render(
+            <Voting pair={newPair} />,
+            container
+        );
+        firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('Sunshine');
+    });
+
 });
